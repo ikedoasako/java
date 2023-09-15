93e6d8809c25ffccddfb38a5188818c7fb656bf6
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-09-15 15:00:14 +0000

    javaで練習7

---
+++ test7.java
@@ -0,0 +1,12 @@
+//文字列の一致を判定
+//equalsメソッド
+
+import java.util.*;
+
+public class test7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); /*標準入力から受け取った文字列*/
+        String name = scan.next(); /*受け取った文字列を変数nameに代入し*/
+        System.out.println("Hello " + name); /*”Hello”という文字列とnameを連結させて表示させる*/
+    }
+}
