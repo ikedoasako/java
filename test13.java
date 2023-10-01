6b85d89aa0e5d5381db2a211e4b1feb8d2573c51
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-10-01 14:38:21 +0000

    javaで練習[2行の簡単な計算]

---
+++ test13.java
@@ -0,0 +1,12 @@
+//計算をする
+
+public class test13 {
+  public static void main(String[] args) {
+    System.out.println("足し算をします");
+    System.out.println(31 + 31);
+    System.out.println("引き算をします");
+    System.out.println(35 - 10);
+    System.out.println("掛け算をします");
+    System.out.println(-5 * 2);
+  }
+}
