0ede1cc2f1ba8f4d4e364b900c3f90279e81fec6
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-10-03 14:48:24 +0000

    javaで練習[簡単な掛け算]

---
+++ Practice1.java
@@ -0,0 +1,16 @@
+//「縦幅3 横幅5 長方形の面積は、15」と表示させる
+
+//（1）変数aに3、変数bに5を入れる
+//（2）変数aとbの掛け算の結果を変数cに入れる
+//（3）最後に変数cを表示させる
+
+public class Practice1 {
+  public static void main(String[] args) {
+
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+
+    System.out.println("縦幅a横幅bの長方形の面積は、" + c);
+  }
+}
