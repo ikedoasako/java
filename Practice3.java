601c3cc32c8889f3c5d114d6fd50e0e8a6172a8d
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-10-07 14:45:36 +0000

    javaで練習[計算の文]

--- Practice3.java
+++ Practice3.java
@@ -1,11 +1,17 @@
 //計算の分
 
-public class Main {
+public class Practice3 {
   public static void main(String[] args) {
+
+//変数宣言の文(int)
     int a;
     int b;
+
+//計算の文(代入)
     a = 20;
+//計算の文
     b = a + 5;
     System.out.println(a);
+    System.out.println(b);
   }
 }
