e90788a2a3cf567ea17d3059f7cc0b37f8557189
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-10-11 14:55:30 +0000

    javaで練習[連取問題2-1]

--- Practice5.java
+++ Practice5.java
@@ -1,12 +1,12 @@
 //　実行結果が「x+yは510」と表示されるコードを
-//「x+yは15と表示させる」
+//「x+yは15」と表示させる為に正しく動作するため修正する
 
 public class Practice5 {
   public static void main(String[] args) {
 
     int x = 5;
     int y = 10;
-    String ans = "x+yは" + X + y;
+    String ans = "x+yは" + (x + y); /*計算して1「15」と表示させる為に（）で囲む*/
     System.out.println(ans);
 
   }
