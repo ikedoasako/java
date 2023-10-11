3f08ea0e575bb57969a94f2d83ab4aab6c3b6658
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-10-11 14:30:10 +0000

    javaで練習[2-1]

---
+++ Practice5.java
@@ -0,0 +1,13 @@
+//　実行結果が「x+yは510」と表示されるコードを
+//「x+yは15と表示させる」
+
+public class Practice5 {
+  public static void main(String[] args) {
+
+    int x = 5;
+    int y = 10;
+    String ans = "x+yは" + X + y;
+    System.out.println(ans);
+
+  }
+}
